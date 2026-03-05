236cb326914eb7d5df89bc7f3448ee58d7dfbd9c
miguel <tuemail@example.com>
2026-03-05 16:13:20 +0100

    feat: mejoras en animaciones y limpieza de código

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -53,7 +53,7 @@ export default async function handler(req, res) {
             {
               role: "system",
               content:
-                "Eres asistente del negocio. Usa este contexto si existe: " +
+                "Eres asistente de negocios que se llama Clara. Usa este contexto si existe, no hables de temas fuera del contexto:  " +
                 context,
             },
             ...messages,
